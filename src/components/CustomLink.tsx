5462ead80ae7f8fd3e8d66bf9a41ba151d5bd30c
Dongyeon Park <26868715+plzfday@users.noreply.github.com>
2024-07-19 11:35:03 +0900

    🔧 feat: Refactor CustomLink component to use Next.js and MUI Link components properly

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,19 +1,10 @@
-import { forwardRef } from 'react';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 import { Link as MUILink, LinkProps as MUILinkProps } from '@mui/material';
 
 type CustomLinkProps = NextLinkProps & MUILinkProps;
 
-const CustomLink = forwardRef<HTMLAnchorElement, CustomLinkProps>(
-  function CustomLink(props, ref) {
-    const { href, as, ...other } = props;
+export default function CustomLink(props: CustomLinkProps) {
+  const { href, ...others } = props;
 
-    return (
-      <NextLink href={href} as={as} passHref>
-        <MUILink ref={ref} {...other} />
-      </NextLink>
-    );
-  }
-);
-
-export default CustomLink;
+  return <MUILink component={NextLink} href={href} {...others} />;
+}
