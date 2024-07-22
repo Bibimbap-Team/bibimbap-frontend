867c001f1a9609406dfda688b6a04ad46b2ea00b
Dongyeon Park <26868715+plzfday@users.noreply.github.com>
2024-07-22 21:34:09 +0900

    ⏫ Update styling to be responsive

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -2,8 +2,8 @@ import Changelog from './Changelog';
 
 export default function Index() {
   return (
-    <div className='grid grid-cols-12'>
-      <div className='p-8 col-span-9'>
+    <div className='flex flex-wrap'>
+      <div className='py-8 pr-8'>
         <div>
           The mission of Polygon is to provide platform for creation of
           programming contest problems. Polygon supports the whole development
@@ -17,13 +17,13 @@ export default function Index() {
           </ul>
         </div>
       </div>
-      <div className='col-span-3 grid grid-rows-2 gap-4'>
-        <div className='border border-primary rounded-md py-2 px-4 bg-primary w-full flex flex-col gap-2'>
+      <div className='flex flex-col gap-4'>
+        <div className='border border-primary rounded-md py-2 px-4 bg-primary flex flex-col gap-2'>
           <p>Registered users: 12345</p>
           <p>Problems total: 12345</p>
           <p>Invokers waiting: 34</p>
         </div>
-        <div className='border border-primary rounded-md py-2 px-4 bg-primary w-full flex flex-col gap-2'>
+        <div className='border border-primary rounded-md py-2 px-4 bg-primary flex flex-col gap-2'>
           <Changelog date='15 Jun 2024' content='Added a new feature' />
           <Changelog date='12 Jul 2014' content='Fixed a bug' />
           <Changelog date='27 Jun 2013' content='Initial release of the app' />
