0058961e74c9b8b1a4b41d8ebfef08a564a61a4f
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-08-06 19:16:24 +0900

    Edit Statement Page

---
+++ src/app/problems/[qid]/statements/EditStatement.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Button, Paper, Stack, TextField, Autocomplete, Link } from '@mui/material';
+
+import { useState } from 'react';
+
+// As same as Polygon, the page link is same for creating new language and editing existed language
+// It can be divided into two pages if needed
+
+export default function EditStatement({ language }: { language: string }) {
+  // cloneClicked variable is used to show the existedLanguageList when the user wants to clone
+  const [cloneClicked, setCloneClicked] = useState(false);
+  return (
+    <Paper>
+      <Stack direction='column' px={4} py={2} gap={2}>
+        <TextField id='prob_name' label='Name' fullWidth />
+        <TextField id='legend' label='Legend' rows={10} multiline fullWidth />
+        <TextField
+          id='input_format'
+          label='Input Format'
+          rows={5}
+          multiline
+          fullWidth
+        />
+        <TextField
+          id='output_format'
+          label='Output Format'
+          rows={5}
+          multiline
+          fullWidth
+        />
+        <TextField id='scoring' label='Scoring' rows={10} multiline fullWidth />
+        <TextField id='notes' label='Notes' rows={5} multiline fullWidth />
+        <TextField id='tutorial' label='Tutorial' rows={10} multiline fullWidth />
+        <Button variant='contained'>Save</Button>
+      </Stack>
+    </Paper>
+  );
+}
